39063e4405a092cc1ad6931b010d3f1f9d7b0737
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-27 18:03:30 +0100

    [add] contenuto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import MainSection from './components/MainSection'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
       <Header />
+      <MainSection />
+      <Footer />
 
 
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Style from './Header.module.css';
 export default function Header() {
 
     return (
-        <header>
+        <header className={Style.header}>
             <h1>Il mio Blog</h1>
         </header>
     )
