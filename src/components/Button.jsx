15f7fe9c97e45261746ff43a166eb2a849e196f8
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-27 18:18:59 +0100

    [fix] button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import style from './Button.module.css'
 
 export default function Button() {
-    return <button className={style.button}>Add to cart</button>
+    return <button className={style.button}>Leggi di più</button>
 }
